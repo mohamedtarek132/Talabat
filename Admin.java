4e0d758224c7d5f5b23c5e5157f74addde56cedf
Omar Mostafa <152614180+OMARMS11@users.noreply.github.com>
2023-12-04 10:07:21 +0200

    Add files via upload

---
+++ Admin.java
@@ -0,0 +1,38 @@
+
+
+public class Admin extends User {
+public  void deleteResturant(){
+
+}
+
+public  void add resturant(){
+
+}
+
+
+public  void addItemToMenu()
+{
+
+}
+
+public void removeItemFromMenu()
+{
+
+}
+
+ public  void displayResturant()
+ {
+
+ }
+
+public  void updateItemPrice()
+{
+
+}
+
+public  void displayResturantMenu(Resturant)
+{
+
+}
+
+}
