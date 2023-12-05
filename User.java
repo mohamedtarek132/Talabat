b9048dd2985449224194a65b23ce827e7623c582
Mohamed Tarek <mohamedtare01234@gmail.com>
2023-12-05 19:07:10 +0200

    changed the address from an array to an arraylist and added the credit card attribute and function and add the searchforitem function

--- User.java
+++ User.java
@@ -1,15 +1,16 @@
 package Talabat;
-
-public class User implements Cloneable{
+import java.util.ArrayList;
+public class User{
     private String first_name;
     private  String lastName;
     private  String email;
     private  String password;
     private  String gender;
     private  int phoneNumber;
-    private  String [] address;
+    private  ArrayList<String> address;
     private  String country;
     private  String id;
+    private ArrayList<CreditCard> creditCards;
 
     public String getFirst_name() {
         return first_name;
@@ -59,11 +60,11 @@ public class User implements Cloneable{
         this.phoneNumber = phoneNumber;
     }
 
-    public String[] getAddress() {
+    public ArrayList<String> getAddress() {
         return address;
     }
 
-    public void setAddress(String[] address) {
+    public void setAddress(ArrayList<String> address) {
         this.address = address;
     }
 
@@ -83,12 +84,9 @@ public class User implements Cloneable{
         this.id = id;
     }
     public void addAddress(String address){
-        String [] new_address = new String [this.address.length + 1];
-        System.arraycopy(this.address, 0, new_address, 0, this.address.length);
-        new_address [this.address.length] = address;
-        this.address = new_address;
+        this.address.add(address);
     }
-    public User[] signUp(String email, String password, User []users){
+    public void signUp(String email, String password, ArrayList<User> users){
         User [] new_users;
         boolean found = false;
         for (User user : users) {
@@ -101,16 +99,13 @@ public class User implements Cloneable{
             if(found){
                throw new SameEmailException();
             }
-            new_users = new User[users.length + 1];
-            System.arraycopy(users, 0, new_users, 0, users.length);
-            new_users[users.length].email = email;
-            new_users[users.length].password = password;
-            return new_users;
+            users.add(new User());
+            users.get(users.size() - 1).setEmail(email);
+            users.get(users.size() - 1).setPassword(password);
         }
         catch(SameEmailException exp){
             System.out.println(exp.getMessage());
         }
-        return users;
     }
     public void logOut(){
         this.address = null;
@@ -123,11 +118,11 @@ public class User implements Cloneable{
         this.password = null;
         this.phoneNumber = 0;
     }
-    public void signIn(String email, String password, User []users) throws EmailOrPasswordException{
+    public void signIn(String email, String password, ArrayList<User> users) throws EmailOrPasswordException{
         boolean found = false;
         int index = -1;
-        for(int i = 0; i < users.length; i++){
-            if(email.equals(users[i].getEmail()) && password.equals(users[i].getPassword())){
+        for(int i = 0; i < users.size(); i++){
+            if(email.equals(users.get(i).getEmail()) && password.equals(users.get(i).getPassword())){
                 found = true;
                 index = i;
                 break;
@@ -136,19 +131,29 @@ public class User implements Cloneable{
         if(!found){
             throw new EmailOrPasswordException();
         }
-        this.address = users[index].address.clone();
-        this.country = users[index].country;
-        this.email = users[index].email;
-        this.gender = users[index].gender;
-        this.id = users[index].id;
-        this.first_name = users[index].first_name;
-        this.lastName = users[index].lastName;
-        this.password = users[index].password;
-        this.phoneNumber = users[index].phoneNumber;
+        this.address = users.get(index).address;
+        this.country = users.get(index).country;
+        this.email = users.get(index).email;
+        this.gender = users.get(index).gender;
+        this.id = users.get(index).id;
+        this.first_name = users.get(index).first_name;
+        this.lastName = users.get(index).lastName;
+        this.password = users.get(index).password;
+        this.phoneNumber = users.get(index).phoneNumber;
     }
-    public Object clone() throws CloneNotSupportedException {
-        User clone = (User) super.clone();
-        clone.address = this.address.clone();
-        return clone;
+    public ArrayList<Item> searchForItem(String name,ArrayList<Restaurant> restaurants){
+        ArrayList<Item> items = new ArrayList<>();
+        for(Restaurant restaurant: restaurants){
+            for(Item item: restaurant.getMenu()){
+                if(item.getName().contains(name)){
+                    items.add(item);
+                }
+            }
+        }
+        return items;
+    }
+    public void addCreditCard(String number, int cvv, String expiry_date, String cardHolder){
+        CreditCard newCreditCard = new CreditCard(number, cvv, cardHolder, expiry_date);
+        this.creditCards.add(newCreditCard);
     }
 }
