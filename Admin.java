f2bd0642c93270098b80d02f7814a0a46dd546b9
Omar Mostafa <152614180+OMARMS11@users.noreply.github.com>
2023-12-06 11:09:14 +0200

    Edit some methods

--- Admin.java
+++ Admin.java
@@ -23,29 +23,27 @@ Resturant[] newRest = new Resturant[resturant.length];
 
 public Resturant setNewResturant()     //this method is a parameter for addResturantToResturants();
 {
-    System.out.println("Enter resturant name ");
-    System.out.println("Enter resturant phone number ");
-    System.out.println("Enter resturant Adress ");
-    System.out.println("Enter resturant category ");
-    System.out.println("Enter resturant  ");
-    System.out.println("Enter resturant name ");
-    System.out.println("Enter resturant name ");
+    System.out.println("Enter resturant name ");             //
+    System.out.println("Enter resturant phone number ");     //
+    System.out.println("Enter resturant Adress ");           //
+    System.out.println("Enter resturant category ");           //this will be in textbox
+    System.out.println("Enter resturant  ");                 //
+    System.out.println("Enter resturant name ");             //
+    System.out.println("Enter resturant name ");             //
 
     Resturant resturant = new resturant(name,phoneNumber,category,etc);
+    return resturant;
 }
 
-public Resturant addResturantToResturants(Resturant allResturants,Resturant newResturant,String,String,){
+public Resturant addResturantToResturants(Resturant[] allResturants,Resturant newResturant,String,String){
 
     for (i:allResturants) {
         if(allResturants[i] == null)
         {
             allResturants[i]=newResturant;
         }
-
     }
     return allResturants;
-
-
 }
 
 
@@ -67,25 +65,33 @@ public  Item addItemToMenu( Resturant resturant,int resturantId,String itemName,
  }
  return resturant.menu;
 
-
-
 }
 
 
 
-public void removeItemFromMenu()
+public Item removeItemFromMenu(Item[] item , int itemIndex)
 {
-
+    Item newItem = new Item[item.length];
+    item[itemIndex]= null;
+    int j=0;
+    for (int i = 0; i < item.length; i++) {
+        if(item[i]==null)
+            continue;
+        newItem[j]= item[j];
+        j++;
+    }
+return newItem;
 }
 
  public  void displayAllResturants ()
  {
-
+int j=0;
    for(i : resturant)
    {
-       if(resturant[i]== null)
+       if(resturant[i] == null)
            continue;
-    System.out.println((i+1) +" "+ resturant[i].name);
+       System.out.println((j+1) +" "+ resturant[i].name);  //this will be in textbox
+       j++;
    }
  }
 
@@ -101,7 +107,7 @@ public  void displayResturantMenu(Resturant)
 {
  for (i : resturant.menu)
   {
-  System.out.println((i+1)+" "+resturant.menu[i].name+" Price -> " resturant.menu[i].price);
+  System.out.println((i+1)+" "+resturant.menu[i].name+" Price -> " resturant.menu[i].price);//this will be in textbox
   }
 
 }
