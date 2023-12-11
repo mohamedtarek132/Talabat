b418d5550f4dd639a14f085f4f11daf4b62303fd
محمد طارق محمد نبهان سيد احمد <122485820+mohamedtarek132@users.noreply.github.com>
2023-12-12 00:26:59 +0200

    Update Admin.java

--- Admin.java
+++ Admin.java
@@ -1,134 +1,148 @@
-//package Talabat;
-//
-//import java.util.ArrayList;
-//
-//public class Admin extends User {
-//public  Restaurant  deleteResturant (Restaurant resturant) {
-//    int restId;
-//    ArrayList<Restaurant> newRest = new ArrayList<>();
-//
-//    System.out.println("Enter resturant id to delete ");
-//
-//    for (Restaurant r : resturant) {
-//
-//        if (r.shopId == restId) {
-//            r = null;
-//            break;
-//        }
-//    }
-//
-//    for (int i = 0; i < resturant.length; i++) {
-//        if(resturant[i]==null)
-//            continue;
-//        else
-//            newRest[i] = resturant[i];
-//    }
-//    return newRest;
-//}
-//
-//public Restaurant setNewRestaurant()     //this method is a parameter for addRestaurantToRestaurants();
-//{
-//    System.out.println("Enter restaurant name ");             //
-//    System.out.println("Enter restaurant phone number ");     //
-//    System.out.println("Enter restaurant Adress ");           //
-//    System.out.println("Enter restaurant category ");           //this will be in textbox
-//    System.out.println("Enter restaurant  ");                 //
-//    System.out.println("Enter restaurant name ");             //
-//    System.out.println("Enter restaurant name ");             //
-//
-//    Restaurant restaurant = new Restaurant();
-//    return restaurant;
-//}
-//
-//public Restaurant addRestaurantToRestaurants(ArrayList<Restaurant> allRestaurants,Restaurant newRestaurant){
-//
-//    for (Restaurant r:allRestaurants) {
-//        if(r == null)
-//        {
-//            r=newRestaurant;
-//        }
-//    }
-//    return allRestaurants;
-//}
-//
-//
-//public  Item addItemToMenu( Restaurant restaurant,int restaurantId,String itemName,int itemPrice)
-//{
-// for (Restaurant i :restaurant) {
-//  if(i.shopId == restaurantId)
-//  {
-//   Item newItem = new Item(itemName,itemPrice);
-//   Item[] arrOfItem = new Item[restaurant.menu.length+1];
-//      for (int i = 0; i < arrOfItem.length; i++) {
-//          arrOfItem[i]= restaurant.menu[i];
-//      }
-//      arrOfItem[arrOfItem.length-1] = newItem;
-//   restaurant.menu = arrOfItem;
-//   break;
-//  }
-//
-// }
-// return restaurant.menu;
-//
-//}
-//
-//
-//
-//public Item removeItemFromMenu(Item[] item , int itemIndex)
-//{
-//    Item newItem = new Item[item.length];
-//    item[itemIndex]= null;
-//    int j=0;
-//    for (int i = 0; i < item.length; i++) {
-//        if(item[i]==null)
-//            continue;
-//        newItem[j]= item[j];
-//        j++;
-//    }
-//return newItem;
-//}
-//
-// public  void displayAllRestaurants ()
-// {
-//int j=0;
-//   for(Restaurant i : restaurant)
-//   {
-//       if(restaurant[i] == null)
-//           continue;
-//       System.out.println((j+1) +" "+ restaurant[i].name);  //this will be in textbox
-//       j++;
-//   }
-// }
-//
-// }
-//
-//public  int updateItemPrice(int newPrice)
-//{
-//
-//  return newPrice;
-//}
-//
-//public  void displayRestaurantMenu(Restaurant)
-//{
-//    int j =0;
-// for (Item i : restaurant.menu)
-//  {
-//  System.out.println((j+1)+"- "+i.name+" Price -> "i.price);//this will be in textbox
-//  }
-//
-//}
-//
-//
-//
-//public  Void  DisplayMostRatedRestaurant(ArrayList<Restaurant> restaurant )
-//{
-//    int i=0;
-//
-//    Collection.sort(restaurant.review.rating);
-//
-//    for(Restaurant r : restaurant)
-//     {
-//       System.out.println((i+1)+"- "+r.name);
-//     }
-//
-//}
+
+package Talabat;
+import java.util.ArrayList;
+import java.util.Comparator;
+
+
+public class Admin extends User  {
+public  ArrayList<Restaurant>  deleteResturant (ArrayList<Restaurant> restaurant, int restId) {
+
+    ArrayList<Restaurant> newRest = new ArrayList<>();
+
+    System.out.println("Enter resturant id to delete ");
+
+    for (Restaurant r : restaurant) {
+
+        if (r.getRestaurantID() == restId) {
+            r = null;
+            break;
+        }
+    }
+
+    for (Restaurant r: restaurant) {
+        if(r == null)
+        {
+            continue;
+        }
+        else
+        {
+            newRest.add(r);
+        }
+    }
+    return newRest;
+}
+
+public Restaurant setNewRestaurant()     //this method is a parameter for addRestaurantToRestaurants();
+{
+    System.out.println("Enter restaurant name ");             //
+    System.out.println("Enter restaurant phone number ");     //
+    System.out.println("Enter restaurant Adress ");           //
+    System.out.println("Enter restaurant category ");           //this will be in textbox
+    System.out.println("Enter restaurant  ");                 //
+    System.out.println("Enter restaurant name ");             //
+    System.out.println("Enter restaurant name ");             //
+
+    Restaurant restaurant = new Restaurant();
+    return restaurant;
+}
+
+public ArrayList<Restaurant> addRestaurantToRestaurants(ArrayList<Restaurant> allRestaurants, Restaurant newRestaurant){
+
+    for (Restaurant r:allRestaurants) {
+        if(r == null)
+        {
+            r=newRestaurant;
+        }
+    }
+    return allRestaurants;
+}
+
+
+
+    public  ArrayList<Item> addItemToMenu(ArrayList<Restaurant> restaurant, int restaurantId, String itemName, String id, String description, int price, String category )
+    {
+        Item newItem = new Item(itemName,id,description,price,category);
+        ArrayList<Item>newMenuAfterAddingItem = new ArrayList<>();
+
+        for (Restaurant r :restaurant) {
+            if(r.getRestaurantID() == restaurantId)
+            {
+
+                newMenuAfterAddingItem = r.getMenu();
+                newMenuAfterAddingItem.add(newItem);
+            }
+
+        }
+        return newMenuAfterAddingItem ;
+
+    }
+
+
+public ArrayList<Item> removeItemFromMenu(ArrayList<Item> item , String itemId)
+{
+    ArrayList<Item> menuAfterDeletingItem =new ArrayList<>() ;
+    int j=0;
+
+    for (Item i : item) {
+        if(i.getId() == itemId)
+        {
+            i.setId(null);
+            break;
+        }
+
+    }
+
+    for (Item i:item) {
+        if(i.getId() == null)
+            continue;
+        else
+            menuAfterDeletingItem.add(i);
+
+    }
+
+
+
+return menuAfterDeletingItem;
+}
+
+ public  void displayAllRestaurants (ArrayList<Restaurant> restaurant)
+ {
+int j=0;
+   for(Restaurant r : restaurant)
+   {
+       if(r == null)
+           continue;
+       System.out.println((j+1) +" "+ r.getName());  //this will be in textbox
+       j++;
+   }
+ }
+
+    public  void displayRestaurantMenu(Restaurant restaurant)
+    {
+        int j =0;
+        for (Item i : restaurant.getMenu())
+        {
+            System.out.println((j+1)+"- "+i.getName()+" Price -> " +i.getPrice());//this will be in textbox
+        }
+
+    }
+    public  int updateItemPrice(int newPrice)
+    {
+
+        return newPrice;
+    }
+
+    public  void displayMostRatedResturant(ArrayList<Restaurant> restaurant)
+    {
+
+    }
+
+
+
+}
+
+
+
+
+
+
