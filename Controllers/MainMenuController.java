950a64887c85fb3cbf453cd8853cb824d299c448
Mohamed Tarek <mohamedtare01234@gmail.com>
2023-12-16 12:44:46 +0200

    h

--- Controllers/MainMenuController.java
+++ Controllers/MainMenuController.java
@@ -64,7 +64,7 @@ public class MainMenuController {
     }
 
     public void switchToWalletScene(ActionEvent event) throws IOException {
-        root = FXMLLoader.load(getClass().getResource("WalletScene.fxml"));
+        root = FXMLLoader.load(getClass().getResource("../Fxmls/Wallet.fxml"));
         stage = (Stage) (((Node) event.getSource()).getScene().getWindow());
         scene = new Scene(root);
         stage.setScene(scene);
--- Controllers/WalletController.java
+++ Controllers/WalletController.java
@@ -2,11 +2,17 @@ package Talabat.Controllers;
 import Talabat.Classes.Payment;
 import Talabat.Classes.Wallet;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.event.ActionEvent;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -56,5 +62,13 @@ public class WalletController{
     public double getbalance()
     {
         return wallet.getBalance();
+    }
+    public void switchToMainMenu(ActionEvent ae) throws IOException {
+        Stage stage;
+        stage = (Stage) ((Node) ae.getSource()).getScene().getWindow();
+        Parent root = FXMLLoader.load(getClass().getResource("../Fxmls/MainMenu.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 }
