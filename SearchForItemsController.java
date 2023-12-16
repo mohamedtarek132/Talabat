c679b2ad0f0b1f9f92cad4d8e1224fd1e55055df
Haoak12 <80491997+Haoak12@users.noreply.github.com>
2023-12-16 05:20:01 +0200

    Add files via upload

---
+++ SearchForItemsController.java
@@ -0,0 +1,72 @@
+package Talabat.Controllers;
+
+import Talabat.Classes.Item;
+import Talabat.Classes.Restaurant;
+
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.awt.event.ActionEvent;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+
+public class SearchForItemsController implements Initializable {
+    public Label searchForItemsTitle;
+    public TextField itemNameTextField;
+    public Label itemNameTextFieldLabel;
+    public ListView<String> itemsListView;
+    public Button searchButton;
+    public Label noItemsLabel;
+    public Button listOfRestaurantsButton;
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    public void goToListOfRestaurants(javafx.event.ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("../Fxmls/ListOfRestaurants.fxml"));
+        stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    String itemName;
+    private ArrayList<Item> items = new ArrayList<>();
+    private ArrayList<String> itemsData = new ArrayList<>();
+
+
+    public void search(javafx.event.ActionEvent event){
+        itemName = itemNameTextField.getText();
+        items = Restaurant.searchForItem(itemName);
+        for (Item item: items){
+            String itemAvailability = "Available";
+            if (!item.getAvailability()){
+                itemAvailability = "Not Available";
+
+            }
+            itemsData.add("Name: " + item.getName() + "\t  " + "Category: " + item.getCategory() + "\t  " +
+                    "Description: " + item.getDescription() + "\t  " + "Price: " + item.getPrice() +
+                    "$\t  Sold " + item.getNumberOfSales() + " times\t  Average Reviews: " +
+                    item.getAvailability());
+        }
+        noItemsLabel.setVisible(false);
+        if(itemsData.isEmpty())noItemsLabel.setVisible(true);
+        itemsListView.getItems().clear();
+        itemsListView.getItems().addAll(itemsData);
+        itemsData.clear();
+    }
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        itemsListView.getItems().addAll(itemsData);
+    }
+}
